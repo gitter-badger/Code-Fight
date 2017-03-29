a48e21a2a9ab4ad9176fa10337c284494605461e
bryan.yang <bryan.yang@ringcentral.com>
2017-03-29 07:42:14 +0000

    add timeout function

--- word_count/test/word_count_test.py
+++ word_count/test/word_count_test.py
@@ -3,7 +3,7 @@
 import unittest2
 from sparktestingbase.testcase import SparkTestingBaseTestCase
 from word_count.solution import answer
-
+from tools.timeout import *
 
 class WordCountTest(SparkTestingBaseTestCase):
     """Simple hell world example test."""
@@ -12,20 +12,23 @@ class WordCountTest(SparkTestingBaseTestCase):
         """Test a parallelize & collect."""
         input = ["hello world"]
         rdd = self.sc.parallelize(input)
-        result = answer(rdd)
+        timeout_ = timeout(answer, 5)
+        result = timeout_(rdd)
         assert result == [('hello', 1), ('world',1)]
-
+    
     def test_order_by_value(self):
         input = ["hello world world"]
         rdd = self.sc.parallelize(input)
-        result = answer(rdd)
+        timeout_ = timeout(answer, 5)
+        result = timeout_(rdd)
         assert result == [('world', 2), ('hello',1)]
 
     def test_same_value(self):
         input = ["hello hello world world"]
         rdd = self.sc.parallelize(input)
-        result = answer(rdd)
+        timeout_ = timeout(answer, 5)
+        result = timeout_(rdd)
         assert result == [('hello', 2), ('world',2)]
-
+   
 if __name__ == "__main__":
     unittest2.main()
